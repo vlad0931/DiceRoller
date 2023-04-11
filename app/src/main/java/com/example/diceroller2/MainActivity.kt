191f6b206716d6f86814c7a88a290375e21349a6
vlad0931 <vasiliev.vladyslav@chnu.edu.ua>
2023-04-11 12:46:24 +0300

    Created class Dice

--- app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
+
 }
